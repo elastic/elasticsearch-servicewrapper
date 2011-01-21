883e44ab04bbcb99765a6bed531aacefc3c9980b
kimchy <kimchy@gmail.com>
2011-01-21 22:14:14 +0200

    upgrade to service wrapper 3.5.6, and add shell script the script type to actually execute

--- service/elasticsearch.bat
+++ service/elasticsearch.bat
@@ -5,7 +5,7 @@ rem Java Service Wrapper command based script.
 
 rem -----------------------------------------------------------------------------
 rem These settings can be modified to fit the needs of your application
-rem Optimized for use with version 3.4.0 of the Wrapper.
+rem Optimized for use with version 3.5.6 of the Wrapper.
 
 rem SET ES_HOME
 set ES_HOME=%~dp0\..\..
@@ -17,15 +17,23 @@ rem The name and location of the Wrapper configuration file.
 rem  (Do not remove quotes.)
 set _WRAPPER_CONF="..\elasticsearch.conf"
 
-rem Do not modify anything beyond this point
-rem -----------------------------------------------------------------------------
+rem _PASS_THROUGH tells the script to pass all arguments through to the JVM
+rem  as is.
+rem set _PASS_THROUGH=true
 
+rem
+rem Resolve the real path of the wrapper.exe
+rem  For non NT systems, the _REALPATH and _WRAPPER_CONF values
+rem  can be hard-coded below and the following test removed.
+rem
 if "%OS%"=="Windows_NT" goto nt
 echo This script only works with NT-based versions of Windows.
 goto :eof
 
 :nt
+rem
 rem Find the application home.
+rem
 rem %~dp0 is location of current script under NT
 set _REALPATH=%~dp0
 
@@ -44,75 +52,42 @@ set _WRAPPER_L_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-ia-64.exe
 goto search
 :search
 set _WRAPPER_EXE=%_WRAPPER_L_EXE%
-if exist "%_WRAPPER_EXE%" goto validate
+if exist "%_WRAPPER_EXE%" goto conf
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
-if exist "%_WRAPPER_EXE%" goto validate
+if exist "%_WRAPPER_EXE%" goto conf
 echo Unable to locate a Wrapper executable using any of the following names:
 echo %_WRAPPER_L_EXE%
 echo %_WRAPPER_EXE%
 pause
 goto :eof
 
-:validate
 rem
-rem Find the requested command.
+rem Find the wrapper.conf
 rem
-for /F %%v in ('echo %1^|findstr "^console$ ^start$ ^pause$ ^resume$ ^stop$ ^restart$ ^install$ ^remove$"') do call :exec set COMMAND=%%v
-
-if "%COMMAND%" == "" (
-    echo Usage: %0 { console : start : pause : resume : stop : restart : install : remove }
-    pause
-    goto :eof
-) else (
+:conf
+set _WRAPPER_CONF="%~f1"
+if not [%_WRAPPER_CONF%]==[""] (
     shift
+    goto :startup
 )
+set _WRAPPER_CONF="%_WRAPPER_CONF_DEFAULT%"
 
 rem
-rem Run the application.
-rem At runtime, the current directory will be that of wrapper.exe
+rem Start the Wrapper
 rem
-call :%COMMAND%
-if errorlevel 1 goto callerror
-goto :eof
+:startup
 
-:callerror
-echo An error occurred in the process.
+rem Collect an parameters
+:parameters
+set _PARAMETERS=%_PARAMETERS% %1
+shift
+if not [%1]==[] goto :parameters
+
+if [%_PASS_THROUGH%]==[] (
+    "%_WRAPPER_EXE%" -c %_WRAPPER_CONF%
+) else (
+    "%_WRAPPER_EXE%" -c %_WRAPPER_CONF% -- %_PARAMETERS%
+)
+if not errorlevel 1 goto :eof
 pause
-goto :eof
 
-:console
-"%_WRAPPER_EXE%" -c %_WRAPPER_CONF%
-goto :eof
-
-:start
-"%_WRAPPER_EXE%" -t %_WRAPPER_CONF%
-goto :eof
-
-:pause
-"%_WRAPPER_EXE%" -a %_WRAPPER_CONF%
-goto :eof
-
-:resume
-"%_WRAPPER_EXE%" -e %_WRAPPER_CONF%
-goto :eof
-
-:stop
-"%_WRAPPER_EXE%" -p %_WRAPPER_CONF%
-goto :eof
-
-:install
-"%_WRAPPER_EXE%" -i %_WRAPPER_CONF%
-goto :eof
-
-:remove
-"%_WRAPPER_EXE%" -r %_WRAPPER_CONF%
-goto :eof
-
-:restart
-call :stop
-call :start
-goto :eof
-
-:exec
-%*
-goto :eof

0bcb55257545f833353df228993c8cb15f1edbc0
Bert Willems <b.willems@premotion.nl>
2013-07-23 13:38:17 +0200

    First parameter is command, as stated in the ES reference guide.
    Command (console, start, stop, install, remove) is passed directly to the wrapper and defaults to console. The config file is now the second parameter.

--- service/elasticsearch.bat
+++ service/elasticsearch.bat
@@ -55,14 +55,25 @@ set _WRAPPER_L_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-ia-64.exe
 goto search
 :search
 set _WRAPPER_EXE=%_WRAPPER_L_EXE%
-if exist "%_WRAPPER_EXE%" goto conf
+if exist "%_WRAPPER_EXE%" goto command
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
-if exist "%_WRAPPER_EXE%" goto conf
+if exist "%_WRAPPER_EXE%" goto command
 echo Unable to locate a Wrapper executable using any of the following names:
 echo %_WRAPPER_L_EXE%
 echo %_WRAPPER_EXE%
 pause
 goto :eof
+
+rem
+rem Determine command to run, defaults to console when no parameters are specified
+rem
+:command
+set _COMMAND=%1
+if not [%_COMMAND%]==[] (
+    shift
+    goto :conf
+)
+set _COMMAND=console
 
 rem
 rem Find the wrapper.conf
@@ -87,9 +98,9 @@ shift
 if not [%1]==[] goto :parameters
 
 if [%_PASS_THROUGH%]==[] (
-    "%_WRAPPER_EXE%" -c %_WRAPPER_CONF%
+    "%_WRAPPER_EXE%" --%_COMMAND% %_WRAPPER_CONF%
 ) else (
-    "%_WRAPPER_EXE%" -c %_WRAPPER_CONF% -- %_PARAMETERS%
+    "%_WRAPPER_EXE%" --%_COMMAND% %_WRAPPER_CONF% -- %_PARAMETERS%
 )
 if not errorlevel 1 goto :eof
 pause
